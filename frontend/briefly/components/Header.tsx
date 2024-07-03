0c67dfe91bb189f124894f7faba58ce983ff7d2a
marcasty <markacastellano2@gmail.com>
2024-07-03 14:28:20 -0400

    don't need header

--- frontend/briefly/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-midjourney_navy text-white p-4">
-      <h1 className="text-2xl font-bold">Briefly</h1>
-    </header>
-  );
-}
